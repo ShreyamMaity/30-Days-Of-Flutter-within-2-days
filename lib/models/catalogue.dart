61c9f09b041f4a30e045d703f327a18e27545be1
Rey <sm8967724231@gmail.com>
2021-12-21 16:51:00 +0530

    json data load, json mapping , dart data class generator, adding circle process inducator

--- lib/models/catalogue.dart
+++ lib/models/catalogue.dart
@@ -1,13 +1,16 @@
-class CatalogueModel{
-  static final items = [Item(
-  id: 1,
-   name: "Iphone12pro",
-    desc: "Iphone12pro max for fun",
-     price: 999,
-      color: "#33505a",
-       image: "https://m.media-amazon.com/images/I/71MHTD3uL4L.jpg")
+import 'dart:convert';
 
-  ];
+class CatalogueModel{
+  static List<Item>? items;
+  // = [Item(
+  // id: 1,
+  //  name: "Iphone12pro",
+  //   desc: "Iphone12pro max for fun",
+  //    price: 999,
+  //     color: "#33505a",
+  //      image: "https://m.media-amazon.com/images/I/71MHTD3uL4L.jpg")
+
+  // ];
   
 
 }
@@ -16,7 +19,7 @@ class CatalogueModel{
 
 
 
-class Item{
+class Item {
   final int id;
   final String name;
   final String desc;
@@ -24,6 +27,89 @@ class Item{
   final String color;
   final String image;
 
-  Item({ required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image });
+  Item({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
+
+
+  
+
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      desc: desc ?? this.desc,
+      price: price ?? this.price,
+      color: color ?? this.color,
+      image: image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'desc': desc,
+      'price': price,
+      'color': color,
+      'image': image,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id']?.toInt() ?? 0,
+      name: map['name'] ?? '',
+      desc: map['desc'] ?? '',
+      price: map['price'] ?? 0,
+      color: map['color'] ?? '',
+      image: map['image'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) => Item.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is Item &&
+      other.id == id &&
+      other.name == name &&
+      other.desc == desc &&
+      other.price == price &&
+      other.color == color &&
+      other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      name.hashCode ^
+      desc.hashCode ^
+      price.hashCode ^
+      color.hashCode ^
+      image.hashCode;
+  }
 }
 
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,32 +1,55 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:test_application/models/catalogue.dart';
 import 'package:test_application/widgets/drawer.dart';
 import 'package:test_application/widgets/itemWidget.dart';
 
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({ Key? key }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogueModel.items[0]);
+  State<Homepage> createState() => _HomepageState();
+}
 
+class _HomepageState extends State<Homepage> {
+  // final dummyList = List.generate(20, (index) => CatalogueModel.items[0]);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson = await rootBundle.loadString("./assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogueModel.items = List.from(productsData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalogue App"),
       ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: ListView.builder(
-            itemCount: dummyList.length,
+          child: (CatalogueModel.items!= null && CatalogueModel.items!.isNotEmpty)? ListView.builder(
+            itemCount: CatalogueModel.items!.length,
             itemBuilder: (context, index) {
               return ItemWidget(
-                item: dummyList[index],
+                item: CatalogueModel.items![index],
               );
             },
-          ),
+          ): Center( child: CircularProgressIndicator(),),
         ),
         drawer: MyDrawer(),
     );
